01c84c9ba4a7624ddb888b0c01b822d6c0fa8481
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2021-12-10 02:19:08 +0900

    Functionalization of the implementation

--- test/test_math.py
+++ test/test_math.py
@@ -1,36 +1,68 @@
 import sys
+from math import gcd as gcd_math
 from random import randint
 
-from ..math import *
+from ..math_lib import *
 
 
 # How to runnning the tests:
 # $ cd ...
 # $ python3 -m library.test.test_math
 
-test_m = randint(1, 1000)
-test_n = randint(1, 1000)
-test_list = [randint(1, 1000) for _ in range(10)]
-print("test_m =", test_m)
-print("test_n =", test_n)
-print("test_list :", test_list)
+def init():
+    test_m = randint(1, 1000)
+    test_n = randint(1, 1000)
+    test_list = [randint(1, 1000) for _ in range(10)]
+    print("test_m =", test_m)
+    print("test_n =", test_n)
+    print("test_list :", test_list)
+    print("---")
+    return test_m, test_n, test_list
+
 
 # n進数変換
-n_conversion = base_n(test_n, 7)
-if base_10(n_conversion, 7) == test_n:
-    print("test success")
-else:
-    print("test failed")
-    sys.exit()
+def test_to_n_base(test_n):
+    tmp =randint(1, 10)
+    n_conversion = base_n(test_n, tmp)
+    if base_10(n_conversion, tmp) == test_n:
+        print("to_n test success")
+    else:
+        print("to_n test failed")
+        sys.exit()
+
 
 # 最大公約数
-gcd_mn = gcd(test_n, test_m)
-print("gcd_mn =", gcd_mn)
+def test_gcd(test_n, test_m, test_list):
+    gcd_mn = gcd(test_n, test_m)
+    if gcd_mn == gcd_math(test_n, test_m):
+        print("gcd test success")
+    else:
+        print("gcd test failed")
+        sys.exit()
+    gcd_list_res = gcd_list(test_list)
+    if gcd_list_res == gcd_math(*test_list):
+        print("gcd_list test success")
+    else:
+        print("gcd_list test failed")
+        sys.exit()
 
-# 最大公約数（リスト）
-gcd_list_res = gcd_list(test_list)
-print("gcd_list_res =", gcd_list_res)
 
 # 最小公倍数
-lcm_mn = lcm(test_n, test_m)
-print("lcm_mn =", lcm_mn)
+def test_lcm(test_n, test_m):
+    lcm_mn = lcm(test_n, test_m)
+    if lcm_mn == test_n * test_m // gcd(test_n, test_m):
+        print("lcm test success")
+    else:
+        print("lcm test failed")
+        sys.exit()
+
+
+def main():
+    test_m, test_n, test_list = init()
+    test_to_n_base(test_n)
+    test_gcd(test_n, test_m, test_list)
+    test_lcm(test_n, test_m)
+
+
+if __name__ == "__main__":
+    main()
