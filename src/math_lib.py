49702c3802d632578925696c08c86668e9a7abba
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-02-06 23:01:41 +0900

    format

--- src/math_lib.py
+++ src/math_lib.py
@@ -6,7 +6,7 @@ def n_sum(n):
 # n進数変換
 def base_n(num_10, n):  # num_10: 10進数, n: 基数
     str_n = ''
-    while num_10:
+    while num_10: # num_10 != 0
         if num_10 % n >= 10:
             return -1
         str_n += str(num_10 % n)
@@ -62,31 +62,31 @@ def cmb(n, r):  # n > 0, r <= n
 
 # エラトステネスの篩
 def sieve(n):
-    is_prime = [True for _ in range(n+1)]
+    is_prime = [True for _ in range(n + 1)]
     is_prime[0] = False
-    for i in range(2, n+1):
-        if is_prime[i-1]:
+    for i in range(2, n + 1):
+        if is_prime[i - 1]:
             j = 2 * i
             while j <= n:
                 is_prime[j-1] = False
                 j += i
-    table = [i for i in range(1, n+1) if is_prime[i-1]]
+    table = [i for i in range(1, n + 1) if is_prime[i - 1]]
     return is_prime, table
 
 
 # 素因数分解
 def factorization(n):
     arr = []
-    temp = n
-    for i in range(2, int(-(-n**0.5//1))+1):
-        if temp % i == 0:
+    tmp = n
+    for i in range(2, int(-(-n ** 0.5 // 1)) + 1):
+        if tmp % i == 0:
             cnt = 0
-            while temp % i == 0:
+            while tmp % i == 0:
                 cnt += 1
-                temp //= i
+                tmp //= i
             arr.append([i, cnt])
-    if temp != 1:
-        arr.append([temp, 1])
+    if tmp != 1:
+        arr.append([tmp, 1])
     if arr == []:
         arr.append([n, 1])
     return arr
@@ -94,22 +94,22 @@ def factorization(n):
 
 # 約数列挙
 def make_divistors(n):
-    lower_divistors, upper_divistors = [], []
+    lower, upper = [], []
     i = 1
     while i * i <= n:
         if n % i == 0:
-            lower_divistors.append(i)
+            lower.append(i)
             if i != n // i:
-                upper_divistors.append(n // i)
+                upper.append(n // i)
         i += 1
-    return lower_divistors + upper_divistors[::-1]
+    return lower + upper[::-1]
 
 
 # xのn乗をmで割った余り
 def pos(x, n, m):
     if n == 0:
         return 1
-    res = pos(x*x % m, n//2, m)
+    res = pos((x ** 2) % m, n // 2, m)
     if n % 2 == 1:
-        res = res*x % m
+        res = res * x % m
     return res
