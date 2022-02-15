77ce80880f92460713e9d8c2e6ee3d229a65356e
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-02-15 17:53:57 +0900

    dijkstra

--- src/data_structure_lib.py
+++ src/data_structure_lib.py
@@ -1,4 +1,5 @@
 from collections import deque
+import heapq
 
 
 #  二分探索
@@ -174,6 +175,21 @@ print(ans)
 # 幅優先探索
 
 # ダイクストラ法
+def dijkstra(adj, n, s=0):  # adj: 隣接行列，n: ノード数，s: 始点ノード
+    INF = 10 ** 9
+    d = [INF] * n # 始点からの最短距離
+    d[s] = 0 # 始点ノードの距離は0
+    seen = [False] * n # 既に訪れたかどうか
+    tmp = [(0, s)] 
+    while tmp:
+        tmp_v = heapq.heappop(tmp)[1]
+        seen[tmp_v] = True
+        for t, c in adj[tmp_v]:
+            if (d[t] > d[tmp_v] + c) & (not seen[t]):
+                d[t] = d[tmp_v] + c
+                heapq.heappush(tmp, (d[t], t))
+    return d
+
 
 # ワーシャルフロイド法
 def warshall_floyd(d):  # d[i][j]: ノードiからノードjへの距離
